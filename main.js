41aaaf9b212c1f2f712d206d5b9e82565bebea7b
Sharrod Hines <sharrodhines@gmail.com>
2017-07-26 08:25:09 -0400

    Initial Setup

---
+++ main.js
@@ -0,0 +1,23 @@
+var app = angular.module('minmax', []);
+
+app.controller('MinMaxCtrl', function($scope, $http) {
+    $scope.formModel = {};
+    
+    $scope.onSubmit = function(valid) {
+        
+        if(valid) {
+            console.log("Hey I'm Submitted");
+            console.log($scope.formModel);
+            
+//            $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+//                success(function(data) {
+//                    console.log(":)")
+//                }).error(function(data) {
+//                    console.log(":(") 
+//            });
+        } else {
+            console.log("Invalid Form!");
+        }
+        
+    };
+});
